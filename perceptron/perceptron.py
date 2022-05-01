37d4ffaa9ebb7ea0b575d1881c3dea66dc22c91e
yanleil <yanleil@cn.ibm.com>
2022-05-01 12:07:27 +0800

    perceptron

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,11 +1,11 @@
 from typing import List
+import random
 
 
 class Perceptron:
     """
     Perceptron binary classifier
     """
-
     def __init__(self, eta: float = 1.0, max_iter: int = 1000):
         """
         Perceptron constructor
@@ -15,9 +15,11 @@ class Perceptron:
         self.eta = eta
         self.max_iter = max_iter
 
+        random.seed(1)
+
     def fit(self, x: List[List[float]], y: List[int]) -> None:
         """
-        Fit is used to data training
+        Fit is used for data training
         after the execution, _w (weight vector) will be produced
         :param x: input, vector list
         :param y: output, class label list
@@ -26,7 +28,9 @@ class Perceptron:
         if len(x) <= 0:
             return
 
-        self._w = [0] * (len(x[0]) + 1)  # initialize weight vector to zeros
+        # initialize weight vector to random integers
+        self._w = [random.randint(-10, 10) for _ in range(len(x[0]) + 1)]
+
         times = 0
         while times < self.max_iter:
             times += 1
@@ -35,10 +39,12 @@ class Perceptron:
                 y_predict = self.predict(xi)
                 if yi - y_predict != 0:
                     errors += 1
-                    for i in range(len(xi)):  # update vector w
+                    for i in range(len(xi)):  # update vector _w
                         self._w[i + 1] += self.eta * (yi - y_predict) * xi[i]
                     self._w[0] += self.eta * (yi - y_predict)
-                print('times: {}, xi: {}, yi: {}, y_predict: {}, _w: {}'.format(times, xi, yi, y_predict, self._w))
+                print(
+                    'times: {}, xi: {}, yi: {}, y_predict: {}, _w: {}'.format(
+                        times, xi, yi, y_predict, self._w))
             if 0 == errors:
                 break
 
@@ -48,7 +54,8 @@ class Perceptron:
         :param x: a single sample input xi
         :return: dot product of vector _w and xi
         """
-        return sum([self._w[i + 1] * xi[i] for i in range(len(xi))]) + self._w[0]
+        return sum([self._w[i + 1] * xi[i]
+                    for i in range(len(xi))]) + self._w[0]
 
     def predict(self, xi: List[float]) -> int:
         """
